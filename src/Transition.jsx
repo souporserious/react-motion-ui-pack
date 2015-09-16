74ddb77b4227cb98cd5ce0b00022c9f56564a663
Travis Arnold <ftntravis@gmail.com>
2015-09-15 21:10:08 -0700

    incorporated Measure into Transition

--- src/Transition.jsx
+++ src/Transition.jsx
@@ -3,16 +3,7 @@ import { TransitionSpring, utils } from 'react-motion';
 import Measure from 'react-measure';
 import { getVendorPrefix } from './utils';
 
-const CSS = {
-  transforms: ['translateX', 'translateY', 'scale', 'scaleX', 'scaleY', 'skewX', 'skewY', 'rotate', 'rotateX', 'rotateY'],
-  transforms3D: ['transformPerspective', 'translateZ', 'scaleZ', 'rotateZ']
-};
-
-// could check for < IE10 support and only include non 3d transforms
-const TRANSFORMS = CSS.transforms.concat(CSS.transforms3D);
-
 class Transition extends Component {
-
   static propTypes = {
     component: PropTypes.string,
     appear: PropTypes.bool,
@@ -31,142 +22,52 @@ class Transition extends Component {
     }
   }
 
-  transform = getVendorPrefix('transform')
-  heights = {}
-
-  _storeChildHeights() {
-    
-    const childNodes = React.findDOMNode(this).children;
-
-    // Array.prototype.map.call(this.slides, child => {
-    //   //child
-    // });
-
-    Children.forEach(this.props.children, (child, i) => {
-      
-      if(!child) return;
-      
-      let childNode = childNodes[i];
-      
-      // set height since it is probably 0 from react-motion
-      childNode.style.height = 'auto';
-
-      // grab height and style from node
-      let height = childNode.offsetHeight;
-      let style = getComputedStyle(childNode);
-
-      // subtract padding if box-sizing is set to anything other than border-box
-      if(style.boxSizing !== 'border-box') {
-        height -= parseInt(style.paddingTop) + parseInt(style.paddingBottom);
-      }
-      
-      // store node height to access later
-      this.heights[child.key] = height;
+  _cachedDimensions = {};
+  
+  componentDidMount() {    
+    window.addEventListener('resize', () => {
+      this.forceUpdate();
     });
-  }
-
-  componentDidMount() {
-    if(this.props.enter.height && this.props.enter.height.val === 'auto') {
-      this._storeChildHeights();
-    }
-  }
-
-  componentDidUpdate() {
-
-    if(this.props.enter.height && this.props.enter.height.val === 'auto') {
-      // need setTimeout because node isn't available for some reason
-      // need to look into why
-      setTimeout(() => {
-        this._storeChildHeights();
-      });
-    }
   }
   
   getEndValues(currValues) {
-
     const { children, appear, enter, leave } = this.props;
     const configs = {};
     const dest = (appear && !currValues) ? leave : enter;
 
-    Children.forEach(children, component => {
-
-      if(!component) return;
-
+    Children.forEach(children, child => {
+      if(!child) return;
       // clone dest object
       let currDest = JSON.parse(JSON.stringify(dest));
-
-      // allow 'auto' value to be passed for height
-      if(dest.height && dest.height.val === 'auto') {
-
-        let height = !currValues ? 0 : this.heights[component.key];
-
-        currDest.height = {
-          val: height
-        };
+      
+      if(currDest.height && dest.height.val === 'auto') {
+        const dimensions = this._cachedDimensions[child.key] || {};
+        const height = dimensions.height || 0;
+        currDest.height = {val: height};
       }
-
-      configs[component.key] = {
-        component: component,
+      
+      configs[child.key] = {
+        component: child,
         dest: currDest
       }
     });
-
     return configs;
   }
 
   willTransition(key, value, endValue, currentValue, currentSpeed) {
-    
     const { leave } = this.props;
-
     return {
       ...value,
       dest: leave
     };
   }
 
-  _mapTransforms(config) {
-    
-    let style = '';
-    
-    for(let prop in config) {
-      
-      if(!config.hasOwnProperty(prop)) return;
-      
-      for(let i = TRANSFORMS.length; i--;) {
-        
-        let transform = TRANSFORMS[i];
-        
-        if(prop === transform) {
-
-          let value = config[prop].val;
-          let unit = '';
-
-          if(prop.indexOf('translate') > -1) {
-            unit = 'px';
-          }
-          else if(prop.indexOf('rotate') > -1 ||
-            prop.indexOf('skew') > -1) {
-            unit = 'deg';
-          }
-
-          style += `${prop}(${value}${unit}) `;
-        }
-      }
-    }
-
-    return style;
-  }
-
   _configToStyle(config) {
-
     let styles = {};
 
     Object.keys(config).map(key => {
-
-      let transformIndex = TRANSFORMS.indexOf(key);
-
-      if(transformIndex > -1) {
-        styles[this.transform] = this._mapTransforms(config);
+      if(key === 'transform') {
+        styles[this.transform] = config[key].val;
       } else {
         styles[key] = config[key].val;
       }
@@ -176,21 +77,28 @@ class Transition extends Component {
   }
 
   render() {
-
     const childrenToRender = (currValues) => Object.keys(currValues).map(key => {
-      
       const currValue = currValues[key];
-
-      return cloneElement(currValue.component, {
-        style: this._configToStyle(currValue.dest)
-      })
+      const { component, dest } = currValue;
+      
+      return(
+        <Measure key={key}>
+          {dimensions => {
+            this._cachedDimensions[component.key] = dimensions;
+            return cloneElement(component, {
+              style: this._configToStyle(dest),
+              dimensions
+            })
+          }}
+        </Measure>
+      );
     });
 
     return(
       <TransitionSpring
-        endValue={::this.getEndValues}
-        willEnter={::this.willTransition}
-        willLeave={::this.willTransition}
+        endValue={this.getEndValues.bind(this)}
+        willEnter={this.willTransition.bind(this)}
+        willLeave={this.willTransition.bind(this)}
       >
         {currValues =>
           React.createElement(
