b284e55d2db03ba48a0dda327875e95a832b8348
Travis Arnold <ftntravis@gmail.com>
2015-11-06 10:49:22 -0800

    oops, stopped mutating props

--- src/to-RM-styles.js
+++ src/to-RM-styles.js
@@ -2,14 +2,14 @@ import { spring } from 'react-motion'
 
 export default function toRMStyles(styles) {
   let rmStyles = {}
-
+  
   Object.keys(styles).forEach(key => {
     let style = styles[key]
     let isObject = (typeof style === 'object')
 
     // check if user passed their own config
     // if not default to regular spring
-    rmStyles[key] = isObject ? style : spring(style)
+    rmStyles[key] = isObject ? {...style} : spring(style)
   })
 
   return rmStyles
