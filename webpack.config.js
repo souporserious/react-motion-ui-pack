3968b05840aa257d2133f0c7334d23835ff5fc17
Travis Arnold <ftntravis@gmail.com>
2016-02-06 17:31:14 -0800

    cleaned up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,23 @@ var path = require('path');
 var nodeModulesDir = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
-    entry: {
-        index: ['webpack/hot/dev-server', './example/index.jsx']
-    },
-    output: {
-        path: './example',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            { test: /\.(js|jsx)/, exclude: /node_modules/, loader: 'babel?stage=0' },
-            { test: /\.scss$/, loader: 'style!css!sass?sourceMap' }
-        ]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    devServer: {
-        contentBase: './example',
-    }
+  entry: {
+    index: ['webpack/hot/dev-server', './example/index.jsx']
+  },
+  output: {
+    path: './example',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+    { test: /\.(js|jsx)/, exclude: /node_modules/, loader: 'babel?stage=0' },
+    { test: /\.scss$/, loader: 'style!css!sass?sourceMap' }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './example',
+  }
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,45 +3,44 @@ var webpack = require('webpack');
 var TARGET = process.env.TARGET || null;
 
 var config = {
-    entry: {
-        index: './src/react-motion-ui-pack.js'
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        publicPath: 'dist/',
-        filename: 'react-motion-ui-pack.js',
-        sourceMapFilename: 'react-motion-ui-pack.sourcemap.js',
-        library: 'Transition',
-        libraryTarget: 'umd'
-    },
-    module: {
-        loaders: [
-            { test: /\.(js|jsx)/, loader: 'babel?stage=0' }
-        ]
-    },
-    plugins: [],
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    externals: {
-        'react': 'React',
-        'react/addons': 'React',
-        'react-motion': 'ReactMotion',
-        'react-measure': 'Measure'
-    },
+  entry: {
+    index: './src/react-motion-ui-pack.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: 'dist/',
+    filename: 'react-motion-ui-pack.js',
+    sourceMapFilename: 'react-motion-ui-pack.sourcemap.js',
+    library: 'Transition',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+    { test: /\.(js|jsx)/, loader: 'babel?stage=0' }
+    ]
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  externals: {
+    'react': 'React',
+    'react-motion': 'ReactMotion',
+    'react-measure': 'Measure'
+  },
 };
 
-if(TARGET === 'minify') {
-    config.output.filename = 'react-motion-ui-pack.min.js';
-    config.output.sourceMapFilename = 'react-motion-ui-pack.min.js';
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false
-        },
-        mangle: {
-            except: ['React', 'ReactMotion', 'Spring', 'TransitionSpring', 'Transition', 'Measure']
-        }
-    }));
+if (TARGET === 'minify') {
+  config.output.filename = 'react-motion-ui-pack.min.js';
+  config.output.sourceMapFilename = 'react-motion-ui-pack.min.js';
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    mangle: {
+      except: ['React', 'ReactMotion', 'Transition', 'Measure']
+    }
+  }));
 }
 
 module.exports = config;
