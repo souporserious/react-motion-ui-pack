96e9c601a83952aec550295d2f50dc6e82d2edd5
Chris Drackett <drackett@mac.com>
2017-10-24 13:25:31 -0700

    typo?
    At least I assume...

--- src/is-element.js
+++ src/is-element.js
@@ -3,7 +3,7 @@ import { isValidElement } from 'react'
 export default function isElement(props, propName, componentName) {
   if (typeof props[propName] !== 'function') {
     if (isValidElement(props[propName])) {
-      return new Error(`${ComponentName} is not an actual Element`)
+      return new Error(`${componentName} is not an actual Element`)
     }
   }
 }
